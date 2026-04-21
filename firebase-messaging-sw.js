c30fcb415c7da65556c49f8d374cc8b149738eb3
kaushalbaitha7 <kaushalbaitha74@gmail.com>
2026-04-21 05:36:49 +0530

    UI update: floating install button, notification bell, slide animation

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts("https://www.gstatic.com/firebasejs/12.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+apiKey: "AIzaSyBM2gM2d24Fp3wH9v0sb0a_UChuz1KvFR8",
+authDomain: "verthink-kkb.firebaseapp.com",
+projectId: "verthink-kkb",
+messagingSenderId: "732567518586",
+appId: "1:732567518586:web:2d9612aa832fa1491b6ac3"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload){
+
+self.registration.showNotification(payload.notification.title,{
+body: payload.notification.body,
+icon:"/icon-192.png"
+});
+
+});
